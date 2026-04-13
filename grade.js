f7d71444f175f1a67970dd5021f6741452a0d046
Shubhanshu Tiwari <shubhanshu.25b15410131@abes.ac.in>
2026-04-13 21:36:14 +0530

    experiment4

---
+++ grade.js
@@ -0,0 +1,18 @@
+// let grade=58;
+// if(grade>75){
+//     console.log("good");
+// }
+// else if(grade>50){
+//     console.log("average");
+// }
+// else{
+//     console.log("poor");
+// }
+let month ="january";
+switch(grade){
+    case "january"
+    console.log("start of the year");
+    break;
+    case "december"
+    console.log("end of the year")
+}
